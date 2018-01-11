db54d756a3dc1d49f948e59808f724182055f162
Admin <Admin@CID202>
2018-01-12 00:36:32 +0500

    Add project files.

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppElec.Startup))]
+namespace AppElec
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
