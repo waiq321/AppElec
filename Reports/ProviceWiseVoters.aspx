fda2680ad97d030c828be6c07489aa7e9e83ad02
waqas <waiq321@gmail.com>
2018-02-11 13:02:13 +0500

    report

---
+++ Reports/ProviceWiseVoters.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MainMaster.master" AutoEventWireup="true" CodeFile="ProviceWiseVoters.aspx.cs" Inherits="Reports_ProviceWiseVoters" %>
+
+<%@ Register assembly="Microsoft.ReportViewer.WebForms, Version=12.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" namespace="Microsoft.Reporting.WebForms" tagprefix="rsweb" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+     <h1>Province and Gender Wise Voters
+     
+        <span style="padding-left: 100px">
+            <asp:Label ID="lblMsg" runat="server" Text=""></asp:Label>
+        </span>
+    </h1
+    <div style="width:60%;margin:0 auto;">
+     <rsweb:ReportViewer ID="ReportViewer1" Width="100%" runat="server" Height="900px" ShowBackButton="False" ShowExportControls="False" ShowFindControls="False" ShowPageNavigationControls="False" ShowPrintButton="False" ShowRefreshButton="False" ShowZoomControl="False" >
+                </rsweb:ReportViewer>
+
+    </div>
+</asp:Content>
+
+
---
+++ Reports/ProviceWiseVoters.aspx.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Reports_ProviceWiseVoters : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+
+            LoadReport();
+
+        }
+    }
+    private void LoadReport()
+    {
+       
+        try
+        {
+
+            DBManager dbMgr = new DBManager();
+            List<SqlParameter> parm = new List<SqlParameter>
+            {
+
+            new SqlParameter("@ElectionId",Request.QueryString["ElectionId"])
+
+            };
+            DataSet ds = dbMgr.ExecuteDataSet("Report_GetProvinceWiseVoters", parm);
+
+            ReportViewer1.LocalReport.ReportPath = Server.MapPath("ProvinceWiseVoters.rdlc");
+            ReportViewer1.LocalReport.DataSources.Clear();
+
+            ReportDataSource repDs = new ReportDataSource();
+            ReportViewer1.LocalReport.DataSources.Add(new Microsoft.Reporting.WebForms.ReportDataSource("DataSetProviceWiseVoters", ds.Tables[0]));
+            ReportViewer1.LocalReport.Refresh();
+
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+
+    
+    protected void btn_save_Click(object sender, EventArgs e)
+    {
+        LoadReport();
+    }
+
+}
